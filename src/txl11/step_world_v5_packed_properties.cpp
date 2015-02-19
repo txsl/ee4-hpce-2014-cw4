ce1c39a5d83177f188b0c9a71e4088ba8fbc6472
Thomas Lim <txl11@ic.ac.uk>
2015-02-19 22:46:16 +0000

    Refactor to v5 to keep names in line with handin spec

--- src/txl11/step_world_v5_packed_properties.cpp
+++ src/txl11/step_world_v5_packed_properties.cpp
@@ -126,7 +126,7 @@ void StepWorldV5PackedProperties(world_t &world, float dt, unsigned n)
 
     cl::Context context(devices);
 
-    std::string kernelSource=LoadSource("step_world_v5_packed_properties.cl");
+    std::string kernelSource=LoadSource("step_world_v5_kernel.cl");
 
     cl::Program::Sources sources;   // A vector of (data,length) pairs
     sources.push_back(std::make_pair(kernelSource.c_str(), kernelSource.size()+1)); // push on our single string
