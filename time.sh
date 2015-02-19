a3cfdb5460eb65fe3555c0ec0946501238495b1c
Thomas Lim (DoC) <txl11@ic.ac.uk>
2015-02-19 22:56:51 +0000

    Shell script not playing nicely :(

--- time.sh
+++ time.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-TASKS="bin/step_world bin/txl11/step_world_v1_lambda bin/txl11/step_world_v2_function bin/txl11/step_world_v3_opencl bin/txl11/step_world_v4_double_buffered bin/txl11/step_world_v5_packed_properties"
+TASKS="bin/step_world bin/txl11/step_world_v3_opencl bin/txl11/step_world_v4_double_buffered bin/txl11/step_world_v5_packed_properties";
 
-TIMES="10 100 1000 10000"
+TIMES="10 100 1000 10000";
 
 for task in $TASKS; do
     for time in $TIMES; do
-        echo "Running ${task} for ${time}"
-        time -p %e (bin/make_world 5000 0.1 1 | eval "${task} 0.1 ${time} 1" > /dev/null)
+	
+	fname=`echo $task | cut -d'/' -f3`;
+	if [ $task == "bin/step_world" ]; then
+	    fname="step_world";
+	fi
+	command="bin/make_world 5000 0.1 1 | ${task} 0.1 ${time} 1 > /dev/null";
+        echo "Running ${fname} for ${time}";
+
+	/usr/bin/time -f'%e' $command > results/data/${fname}_${time}.csv;
+#	time `eval"$command"`;
+#        /usr/bin/time -f %e -p sh -c $command >> results/data/${fname}_${time}.csv;
     done
 done
 
-
-# time (bin/make_world 5000 0.1 1 | bin/step_world 0.1 1000 1 > /dev/null)
-# time (bin/make_world 5000 0.1 1 | bin/dt10/step_world_v3_opencl 0.1 1000 1 > /dev/null)
-# time (bin/make_world 5000 0.1 1 | bin/dt10/step_world_v4_double_buffered 0.1 1000 1 > /dev/null)
-# time (bin/make_world 5000 0.1 1 | bin/dt10/step_world_v5_packed_properties 0.1 1000 1 > /dev/null)
